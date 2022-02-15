d115f15f2c789ba0eee65bab9ca61fef6b87200e
Joel <kustemo.joel@elev.ga.ntig.se>
2022-02-15 21:49:26 +0100

    d

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-const flipper = document.querySelectorAll(".card");
+let flipper = document.querySelectorAll(".card");
+let deck = document.querySelector(".deck");
+const cardArray = [
+  "front1.jpg",
+  "front1.jpg",
+  "front2.jpg",
+  "front2.jpg",
+  "front3.jpg",
+  "front3.jpg",
+  "front4.jpg",
+  "front4.jpg",
+  "front5.jpg",
+  "front5.jpg"
+];
 
-//flipper.addEventListener("click", cardFlipper);
+const cards = document.querySelector(".card");
 
-flipper.forEach((cards) => cards.addEventListener("click", cardFlipper));
+let matched = [];
+let flipped = [];
+
+flipper.forEach(card => {
+  card.addEventListener("click", cardFlipper)
+});
 
 function cardFlipper() {
   this.classList.toggle("card-front");
+}
+
+
+const random = () => {
+  const cardDeck = cardArray;
+  console.log(cardDeck)
+  cardArray.sort(()=> Math.random() - 0.5)
+};
+
+random();
+
+
+
+function changeMode(modeIndex){
+  
+  document.querySelector(".startScreen").style.display="none";
+
+  document.querySelector("#mode" + modeIndex).style.display="block";  
 }
